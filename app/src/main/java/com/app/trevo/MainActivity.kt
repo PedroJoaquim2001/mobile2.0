dce235558f5d83067bea55019f930b7d0ba26220
pedro joaquim <pedro.joaquim2001@outlook.com>
2023-07-26 16:33:46 -0300

    projeto inicial

---
+++ app/src/main/java/com/app/trevo/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.app.trevo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
